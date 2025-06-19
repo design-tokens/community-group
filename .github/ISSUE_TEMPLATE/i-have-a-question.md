95b534d27597f50a7aab29614b905318cc8ee309
Drew Powers <drew@pow.rs>
2025-06-18 22:14:06 -0600

    Build technical reports into eleventy site

--- .github/ISSUE_TEMPLATE/i-have-a-question.md
+++
@@ -1,7 +0,0 @@
----
-name: I have a question
-about: General education e.g. “How do I do this?” or “Can this template do this?”
-title: ''
-labels: question, education
-assignees: ''
----
--- _includes/assets/js/inline.js
+++
@@ -1,9 +0,0 @@
-if (window.netlifyIdentity) {
-  window.netlifyIdentity.on("init", user => {
-    if (!user) {
-      window.netlifyIdentity.on("login", () => {
-        document.location.href = "/admin/";
-      });
-    }
-  });
-}
