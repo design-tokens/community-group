b3afd4824db4a3fb308e2d4febd6403d497b2271
Louis Chenais <lchenais@gmail.com>
2025-09-24 16:44:32 +0200

    chore(): Move from 11ty to Astro (#309)

---
+++ www/src/site.config.ts
@@ -0,0 +1,53 @@
+export const headerLinks: { path: string; title: string }[] = [
+  {
+    path: '/',
+    title: 'Home',
+  },
+  {
+    path: '/glossary/',
+    title: 'Glossary',
+  },
+  {
+    path: '/blog/',
+    title: 'Blog',
+  },
+  {
+    path: '/press-kit/',
+    title: 'Press Kit',
+  },
+  {
+    path: '/technical-reports/',
+    title: 'Technical Reports',
+  },
+];
+
+export const footerLinks: { path: string; title: string }[] = [
+  {
+    path: 'https://www.w3.org/community/design-tokens',
+    title: '<abbr title="World Wide Web Consortium">W3C</abbr>&nbsp;Group',
+  },
+  {
+    path: 'https://github.com/design-tokens/',
+    title: 'GitHub',
+  },
+  {
+    path: 'https://x.com/DesignTokens',
+    title: 'X',
+  },
+  {
+    path: 'https://bsky.app/profile/designtokens.org',
+    title: 'Bluesky',
+  },
+  {
+    path: 'https://www.linkedin.com/company/design-tokens-community-group/',
+    title: 'LinkedIn',
+  },
+  {
+    path: 'https://opencollective.com/design-tokens',
+    title: 'Donate',
+  },
+  {
+    path: 'https://netlify.com',
+    title: 'Deploys by Netlify',
+  },
+];
