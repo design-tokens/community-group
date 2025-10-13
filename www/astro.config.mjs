4c3d451211f1215ca3372bf549fa75c8e1b8961c
Drew Powers <drew@pow.rs>
2025-10-12 22:07:30 -0600

    fix: fix redirects

--- www/astro.config.mjs
+++ www/astro.config.mjs
@@ -13,25 +13,6 @@ export default defineConfig({
     icon({ iconDir: 'src/assets/vectors' }),
   ],
   prefetch: true,
-  redirects: {
-    // Note: only needed for dev. The prod site handles these automatically
-    // TODO: investigate moving URLs out of public/ and into src/pages/. public/ was a remnant of pre-Astro setup.
-    '/TR/drafts/': '/TR/drafts/index.html',
-    '/TR/drafts/color/': '/TR/drafts/color/index.html',
-    '/TR/drafts/format/': '/TR/drafts/format/index.html',
-    '/TR/drafts/resolver/': '/TR/drafts/resolver/index.html',
-    '/TR/first-editors-draft/': '/TR/first-editors-draft/index.html',
-    '/TR/first-editors-draft/format/':
-      '/TR/first-editors-draft/format/index.html',
-    '/TR/second-editors-draft/': '/TR/second-editors-draft/index.html',
-    '/TR/second-editors-draft/format/':
-      '/TR/second-editors-draft/format/index.html',
-    '/TR/third-editors-draft/': '/TR/third-editors-draft/index.html',
-    '/TR/third-editors-draft/color/':
-      '/TR/third-editors-draft/color/index.html',
-    '/TR/third-editors-draft/format/':
-      '/TR/third-editors-draft/format/index.html',
-  },
   vite: {
     css: {
       preprocessorOptions: {
