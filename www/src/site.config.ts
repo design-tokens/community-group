80b0f97ed68baa8717409cf463b56e01b798035e
Louis Chenais <lchenais@gmail.com>
2025-10-16 07:08:26 +0200

    Make the website responsive (#324)

--- www/src/site.config.ts
+++ www/src/site.config.ts
@@ -3,7 +3,7 @@ export const menu = {
     { path: '/technical-reports/', title: 'Technical Reports' },
     { path: '/contribute/', title: 'Contribute' },
     { path: '/blog/', title: 'Blog' },
-    { path: '/about/', title: 'About' },
+    { path: '/about', title: 'About' },
     { path: 'https://github.com/design-tokens', title: 'GitHub' },
   ],
   about: [
@@ -20,19 +20,37 @@ export const menu = {
     { path: 'https://opencollective.com/design-tokens', title: 'Donate' },
   ],
   social: [
-    { path: 'https://github.com/design-tokens', title: 'DTCG on GitHub' },
+    {
+      path: 'https://github.com/design-tokens',
+      title: 'DTCG on GitHub',
+      icon: 'github',
+      label:
+        'Design Tokens Community Group GitHub repository (opens in a new tab)',
+    },
     {
       path: 'https://www.w3.org/community/design-tokens',
       title: 'DTCG W3C Community Group',
+      icon: 'w3c',
+      label:
+        'Design Tokens Community Group W3C Community Group page (opens in a new tab)',
     },
-    { path: 'https://x.com/DesignTokens', title: 'DTCG on X' },
+    {
+      path: 'https://x.com/DesignTokens',
+      title: 'DTCG on X',
+      icon: 'x',
+      label: 'Follow us on X (opens in a new tab)',
+    },
     {
       path: 'https://bsky.app/profile/designtokens.org',
       title: 'DTCG on Bluesky',
+      icon: 'bluesky',
+      label: 'Follow us on Bluesky (opens in a new tab)',
     },
     {
       path: 'https://opencollective.com/design-tokens',
       title: 'DTCG on OpenCollective',
+      icon: 'open-collective',
+      label: 'Support us on OpenCollective (opens in a new tab)',
     },
   ],
 };
