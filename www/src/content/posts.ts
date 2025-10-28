f9db87248b87afa15f73d37b7d4e72b181a6a5c8
Drew Powers <drew@pow.rs>
2025-10-28 10:12:22 -0700

    fix: missing img (#344)
    * fix: missing img
    
    * fix lint

--- www/src/content/posts.ts
+++ www/src/content/posts.ts
@@ -50,5 +50,5 @@ export const posts: Post[] = [
     date: '2025-10-28',
     author: 'Kaelig Deloumeau-Prigent',
     url: 'https://www.w3.org/community/design-tokens/2025/10/28/design-tokens-specification-reaches-first-stable-version/',
-  }
+  },
 ];
